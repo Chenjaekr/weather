4beee1ca0a7061d05c918dba6ebe309a2af56725
Chenjaekr <chenjaekr_freedom@qq.com>
2016-07-13 10:33:36 +0800

    无关痛痒的bug

---
+++ weather (2).py
@@ -0,0 +1,62 @@
+#-*-coding:utf-8-*-
+
+import urllib2
+# import sys
+# reload(sys)
+# sys.setdefaultencoding('utf8')
+import json
+# import chardet
+
+a = True
+
+while a:
+
+    print '输入想要查询的城市(拼音或汉字)：'
+    city = raw_input()
+
+    web = urllib2.Request('http://apis.baidu.com/heweather/weather/free?city=%s' % city)
+    web.add_header("apikey", "5c7e53c9c2e3dad22afdd1b34ebd4510")
+    web_result = urllib2.urlopen(web)
+    content = web_result.read()
+
+
+
+
+    data = json.loads(content)
+    result = data['HeWeather data service 3.0']
+
+
+    result = result[0]
+    # print result
+    try:
+        print '城市：', result['basic']['city']
+    except:
+        print '无法查询该城市'
+        continue
+    try:
+        print '天气：', result['now']['cond']['txt']
+        print '体感温度：', result['now']['fl']
+        print '实时气温：', result['now']['tmp']
+        print '相对湿度：', result['now']['hum'],'%'
+    except:
+        print '没有天气数据'
+    try:
+        print '空气质量(PM2.5)：', result['aqi']['city']['aqi']
+    except:
+        print '没有AQI数据'
+    print '- - - - - - - - - - - - - - - - - - - - - - - '
+
+    try:
+        print '三小时天气预报：' , result['hourly_forecast'][0]['date']
+        for i in result['hourly_forecast']:
+            print '时间：', i['date'],'\n','温度：', i['tmp'],'℃,', '降水概率：', i['pop'], '%'
+            print '----------------------------------------------'
+        print '更新时间：', result['basic']['update']['loc']
+    except:
+        print '没有三小时天气情况'
+    b = raw_input('是否继续查询？(Y/N)\n')
+    print b
+    if (b == 'Y') or (b == 'y'):
+        continue
+    else:
+        a = False
